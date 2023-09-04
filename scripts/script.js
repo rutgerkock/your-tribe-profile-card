173b4aeb3e4b61fc523c234925bc695a56056623
rutgerkock <kock.rutger@gmail.com>
2023-09-04 17:02:13 +0200

    Updates
    Volledig alles anders gedaan want ja

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,20 @@
-let p1 = document.getElementById("p1");
-function ChangeText () {
-    p1.innerHTML = "- Houten: 10km onder utrecht<br>- Reistijd: 60-90 minuten<br>- Vervoer: Fiets > Trein> Metro";}
+let article = document.getElementById("article");
+let text = document.getElementById("text");
 
-function ChangeText2 () {
-    p1.innerHTML = "- Hoogvliet: Supermarkt<br>- Uren: 8-20 per week";}
+function Button1() {
+    article.style.backgroundColor = "#bff5d4";
+    text.innerHTML = "<dt>Leeftijd: 18</dt><br><dt>Werk: Hoogvliet</dt><br><dt>School</dt><li>HAVO</li><li>HU ICT</li></div>";
 
-function ChangeText3 () {
-  p1.innerHTML = "- Leeftijd: 18 jaar<br>- Familie: 2 zussen, 1 broer";}
-
-function ChangeText4 () {
-  p1.innerHTML = "- Mountainbike: Zeist<br>- Racefiets: Amsterdam rijnkanaal";}
-
-function ChangeText5 () {
-  p1.innerHTML = "- Auto's: Circuit Zandvoort<br>- Vogels<br>- Vakantie";}
+}
+function Button2() {
+  article.style.backgroundColor = "#5CDB95";
+  text.innerHTML = "<dt>Woonplaats: Houten (10km onder Utrecht)</dt><br><dt>Reistijd: 60-90 minuten</dt><br><dt>Vervoer:</dt><li>Fiets</li><li>Trein</li><li>Metro</li></div>";
+}
+function Button3() {
+  article.style.backgroundColor = "#379683";
+  text.innerHTML = "<dt>Mountainbike: Zeist</dt><br><dt>Racefiets: Amsterdam Rijnkanaal</dt></div>";
+}
+function Button4() {
+  article.style.backgroundColor = "#05386B";
+  text.innerHTML = "<dt>Auto's</dt><br><dt>Vogels</dt><br><dt>Vakantie</dt></div>";
+}
