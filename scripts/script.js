5ed9a5d19fcf7b08552588e3ca31e8653e710f48
rutgerkock <kock.rutger@gmail.com>
2023-09-04 12:52:32 +0200

    Updates knoppen
    Alle knoppen hebben nu een functie

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+let p1 = document.getElementById("p1");
+function ChangeText () {
+    p1.innerHTML = "- Houten: 10km onder utrecht<br>- Reistijd: 60-90 minuten<br>- Vervoer: Fiets > Trein> Metro";}
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+function ChangeText2 () {
+    p1.innerHTML = "- Hoogvliet: Supermarkt<br>- Uren: 8-20 per week";}
+
+function ChangeText3 () {
+  p1.innerHTML = "- Leeftijd: 18 jaar<br>- Familie: 2 zussen, 1 broer";}
+
+function ChangeText4 () {
+  p1.innerHTML = "- Mountainbike: Zeist<br>- Racefiets: Amsterdam rijnkanaal";}
+
+function ChangeText5 () {
+  p1.innerHTML = "- Auto's: Circuit Zandvoort<br>- Vogels<br>- Vakantie";}
