68a29a3f65dba148c233253e1c61b7b6d7d10cf8
rutgerkock <kock.rutger@gmail.com>
2023-09-05 12:13:55 +0200

    Updates
    knoppen en lettertypes aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,29 @@
 let article = document.getElementById("article");
 let text = document.getElementById("text");
+let text2 = document.getElementById("text2");
 let afbeelding = document.getElementById("afbeelding");
 
 function Button1() {
   article.style.backgroundColor = "#bff5d4";
   text.innerHTML = "<dt>Leeftijd: 18</dt><br><dt>Werk: Hoogvliet</dt><br><dt>School</dt><li>HAVO</li><li>HU ICT</li></div>";
   afbeelding.src="https://lh3.googleusercontent.com/pw/AIL4fc_sdjSBRDlXihgHxxViE1Lp9CZUogdQEpHqHdPIuk0Q6K3mhb3E9ehB_-s3IxtLfqoLpLLUnYwmA8SAP_4L021h1se8QyUzkOgsHa06KgKpafSThzbUT6MCGda6XaOnMZdx9tgOnfbJdeYjya7U-4spx-QDnau-41XiGQHV8IE5B_SJg7E85Z5JXYQIpCfoGIRYbrjzKJGNPznssVkSk7b6yCdz0wk3k428G5FZL-eyo0ut0qBBYwQYRnjy13Ru3PIVe03yl6YBkSy-gC1RDcbs_yjhgFA1pJeKAzaUdwHBee1T3p3vTJ4mshRtYCh7xhCv6xMHUt4L0FCRUq_VFl14B7MeAUw_N22rFnmKjUmGHQFL3RByz2BiyoZ6paWhVaiCOHLQMFfBtwMPAgGpgog9qAbYiVJa6fPesqXQx8fY-q3hxLK6NJs3Xa3_GxYAGH9Xyr-lrbKdV5jHVa6By9tAvvS6HdFTogoskp6-MsUnM75EKCJ20QhOLQph1BQ95azyUCE69LXaxdVA-Q0_tmJwmaibb0pIMWBaORpIqgf2w8QHG9y7VMoq14IBt_j4vfMFwbiJJnJMnq-k0WeSR9r-0J3VOwv2eqIWgklVMiDKqV5yMmyb4DsXMYPi2cOpF6OQXrAd4bnoPO8LWYPsDR4pZ8KVmbu0Jsol0fwgNSTJc9feneBGgyFIfMuaWTF73Ysyn672IaIQ057Fgr6NlDHOJnMQrg2QEOikjq0gkABLOhUDWVn9GqJ4cL914YwQepJpUkN7OPphi-WOrh3H92q5FG-OjXcvS_riC2vJ8-0qgIqXVww6vcMc8DycvCMAXEf2jgcS0-Rho9LlLSw9kkMNheSZGKD1geAHQVLyd8J4qak09UheSk2J5osYmzMJLRWfsXr0WsQcwKI5P0U_A_I=w669-h1004-s-no?authuser=0";
+  text2.style.color = "#05386b"
 }
 function Button2() {
   article.style.backgroundColor = "#5CDB95";
   text.innerHTML = "<dt>Woonplaats: Houten (10km onder Utrecht)</dt><br><dt>Reistijd: 60-90 minuten</dt><br><dt>Vervoer:</dt><li>Fiets</li><li>Trein</li><li>Metro</li></div>";
   afbeelding.src="https://www.zoekplaats.nl/kaartje-MTU2LjEyMDcuMEhvdXRlbg==";
+  text2.style.color = "#05386b"
 }
 function Button3() {
   article.style.backgroundColor = "#379683";
   text.innerHTML = "<dt>Mountainbike: Veluwe</dt><br><dt>Racefiets: Amsterdam Rijnkanaal</dt></div>";
   afbeelding.src="https://mtb-nationaalparkveluwezoom.nl/wp-content/uploads/2022/06/route-06-2022-1.png";
+  text2.style.color = "#090c0a"
 }
 function Button4() {
   article.style.backgroundColor = "#05386B";
   text.innerHTML = "<dt>Auto's</dt><br><dt>Vogels</dt><br><dt>Vakantie</dt></div>";
   afbeelding.src="https://lh3.googleusercontent.com/pw/AIL4fc_TW-1CiJyN-eeXzMO-n93zVyqUS6wBd6toALuQCSjvE_l3vkrk318ZO4yUFZkeCNS-dW7DKNvbFQYB_wxNsbDWLLzdOthaVWO-98vnbCSke79FtjoTsQwq9ed6TYcnBv22DOt9l2zxMGwzxb1pG1YyKw=w666-h1004-s-no?authuser=0";
+  text2.style.color = "#bff5d4"
 }
