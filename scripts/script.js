60151b9c0f93b6a3f52d712d924db1b3669be629
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2023-09-15 20:07:06 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,4 @@ const toggle = document.getElementById('toggle');
 toggle.addEventListener('click', () => {
   const frontDiv = document.querySelector('.front');
   frontDiv.classList.toggle('moved');
-
-  setTimeout(() => {
-    frontDiv.classList.remove('moved');
-  }, 5000);});
+});
