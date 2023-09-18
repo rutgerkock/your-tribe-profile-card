df9640f5d645df0028c2782c0933c7eda6236b93
rutgerkock <kock.rutger@gmail.com>
2023-09-18 14:31:54 +0200

    svg + rocket added
    Icoontjes nu svg's en een raket toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,6 @@ const toggle = document.getElementById('toggle');
 toggle.addEventListener('click', () => {
   const frontDiv = document.querySelector('.front');
   frontDiv.classList.toggle('moved');
+  const rocket = document.querySelector('#rockets');
+  rocket.classList.toggle('rocketAnimation');
 });
